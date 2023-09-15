80677230c36ec9067f0f0e944d6527577f699b0d
ZahaEssa <shamoonzahabiy@gmail.com>
2023-09-15 19:59:33 +0300

    Added database connections

---
+++ includes/db_blog.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE blog_db;
+ 
+CREATE TABLE blog_writing(
+authorname VARCHAR(80),
+articletitle VARCHAR(150),
+articletext longtext,
+publicationdate TIMESTAMP DEFAULT current_timestamp
+);
+
+CREATE TABLE IF NOT EXISTS verify (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(80) NOT NULL,
+    token VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    expiry_time DATETIME NOT NULL,
+    authorusername VARCHAR(50),
+authorpassword VARCHAR(200),
+authorconfirmpassword VARCHAR(200)
+
+);
+ALTER TABLE verify
+ADD is_verified INT DEFAULT 0;
+
+drop table author;
+select*from verify;
+select*from author;
