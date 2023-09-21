fac1c498080a169ba93fe229563d2e8853258a6e
K-MaryAnne <maryanne.kariuki.k@gmail.com>
2023-09-21 11:18:16 +0300

    Edited signinprocess to include class SignInManager, connection to database construct and signin method

--- processes/signinprocess.php
+++ processes/signinprocess.php
@@ -1,36 +1,51 @@
 <?php
 require_once "../includes/connect.php";
 
-session_start();
-unset($_SESSION["data"]);
+class SignInManager
+{
+    private $con;
 
+    public function __construct($dbConnection)
+    {
+        $this->con = $dbConnection;
+    }
+
+    public function signIn($authorUsername, $password)
+    {
+        $enteredUsername = mysqli_escape_string($this->con, $authorUsername);
+        $enteredPassword = mysqli_escape_string($this->con, $password);
+
+        $findAuthor = "SELECT * FROM verify WHERE authorusername = '$enteredUsername' LIMIT 1";
+
+        $findAuthorRes = $this->con->query($findAuthor);
+        if ($findAuthorRes->num_rows > 0) {
+            $authorRow = $findAuthorRes->fetch_assoc();
+            $storedPassword = $authorRow["authorpassword"];
+            if (password_verify($enteredPassword, $storedPassword)) {
+                session_start();
+                $_SESSION["data"] = $authorRow;
+                header("Location: ../blogsubmission.php");
+                exit();
+            } else {
+                $error_message = "Incorrect password";
+                header("Location: ../signin.php?error=" . urlencode($error_message));
+                exit();
+            }
+        } else {
+            $error_message = "Incorrect username";
+            header("Location: ../signin.php?error=" . urlencode($error_message));
+            exit();
+        }
+    }
+}
+
+// Usage:
+$signInManager = new SignInManager($con);
 
 if (isset($_POST["Signin"])) {
-    $entered_username = mysqli_escape_string($con, $_POST["authorusername"]);
-    $entered_password = mysqli_escape_string($con, $_POST["password"]);
-
-    $find_author = "SELECT * FROM author WHERE authorusername = '$entered_username' LIMIT 1";
-    
-    $find_author_res = $con->query($find_author);
-    if($find_author_res->num_rows > 0){
-        $author_row = $find_author_res->fetch_assoc();
-        $stored_password = $author_row["authorpassword"];
-        if(password_verify($entered_password, $stored_password)){
-            $_SESSION["data"] = $author_row;
-             header("Location: ../blogsubmission.php"); 
-             exit();
-        }else{
-            $error_message = "Incorrect password";
-            header("Location: ../signin.php?error=" . urlencode($error_message)); 
-            exit();
-            }     
-    }else{
-            $error_message = "Incorrect username";
-            header("Location: ../signin.php?error=" . urlencode($error_message)); 
-            exit();
-    }
-    }else{
-        header("Location: ../signin.php");
-        exit();
-    }
+    $signInManager->signIn($_POST["authorusername"], $_POST["password"]);
+} else {
+    header("Location: ../signin.php");
+    exit();
+}
 ?>
