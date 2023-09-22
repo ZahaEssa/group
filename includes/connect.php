9d9c1a8094eb6ab0792c19f30d19f0cb39709e72
ZahaEssa <shamoonzahabiy@gmail.com>
2023-09-22 12:28:18 +0300

    updated verify database

--- includes/connect.php
+++ includes/connect.php
@@ -24,7 +24,7 @@ class Connect {
 
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "786110Zahabiya";
 $db = "blog_db";
 
 $dbConnection = new Connect($servername, $username, $password, $db);
